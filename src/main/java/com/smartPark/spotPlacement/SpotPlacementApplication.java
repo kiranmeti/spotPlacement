07402b3dadb861edfa805b3a0b8586977302293d
kiranmeti <kiranmeti367@gmail.com>
2020-04-16 12:25:51 +0530

    "added spot placement application file"

---
+++ src/main/java/com/smartPark/spotPlacement/SpotPlacementApplication.java
@@ -0,0 +1,14 @@
+package com.smartPark.spotPlacement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotPlacementApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotPlacementApplication.class, args);
+	}
+
+}
